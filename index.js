11a52c94353388c4aae95a5e4843dad98d0b7f59
deepu4551621 <77355959+deepu4551621@users.noreply.github.com>
2024-04-04 18:42:43 +0530

    cors update

--- index.js
+++ index.js
@@ -3,12 +3,18 @@ const express = require('express');
 const bodyParser = require('body-parser')
 const port = process.env.PORT || 6000;
 const app = express();
+const cors=require('cors')
 app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
     extended: true,
   })
 );
+// middleware
+app.use(cors({
+  origin: 'http://localhost:3000',
+  credentials:true
+}));
 app.get('/', require('./routes/authRoutes'));
 
 app.listen(port, () => {
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,14 +1,8 @@
 const express = require('express')
 const router = express.Router()
-const cors = require('cors')
 const Home = require('./controllers/Home')
 const Login =require('./controllers/Login')
 const {createUser, getUserById, getUsers, deleteUser, updateUser} = require('./controllers/query')
-// middleware
-router.use(cors({
-    origin: 'http://localhost:3000',
-    credentials:true
-}));
 
 router.get('/',Home)
 router.post('/login', Login)
